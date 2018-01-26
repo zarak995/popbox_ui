8192084910d808216dc2cfbf1f839a8cd4c118d2
Njabulo Thwala <njabulo.thwala@symbiotics.co.za>
2018-01-27 01:05:18 +0200

    "Changed avatar model"

--- src/app/models/Avatar.ts
+++ src/app/models/Avatar.ts
@@ -1,11 +1,11 @@
 import { User } from './user';
 export interface IAvatar {
-    id: String;
+
     name: String;
     user: String;
 }
 export class Avatar implements IAvatar {
-    constructor(public id: String, 
-        public name: String, 
+    id: String;
+    constructor(public name: String,
         public user: String) { }
 }
