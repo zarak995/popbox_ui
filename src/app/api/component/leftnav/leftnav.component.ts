b93753a2163e4627727a15b57eb3a930ef1400bb
Njabulo Thwala <njabulo.thwala@symbiotics.co.za>
2018-01-27 01:06:05 +0200

    "Displaying only 3 top chats"

--- src/app/api/component/leftnav/leftnav.component.ts
+++ src/app/api/component/leftnav/leftnav.component.ts
@@ -4,7 +4,7 @@ import { Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { ChatService } from '../chat/chat.service';
 import { setInterval } from 'timers';
-
+import { Chat } from '../../../models/Chat';
 
 @Component({
   selector: 'app-leftnav',
@@ -16,9 +16,10 @@ export class LeftnavComponent implements OnInit {
   constructor(private leftnavService: LeftnavService, private chatService: ChatService) { }
   token: String = window.localStorage.getItem('token');
   headers: Headers = new Headers({ 'content-type': 'application/json', 'authorization': this.token });
-  listOfTopChats = [];
+  listOfTopChats: Chat[] = [];
 
   ngOnInit() {
+    this.listOfTopChats = []
     this.getTopChats();
     setInterval(() => {
       this.getTopChats();
@@ -30,9 +31,9 @@ export class LeftnavComponent implements OnInit {
     this.leftnavService.getTopChats(this.headers)
       .map(res => res.json())
       .subscribe(data => {
-        data.forEach(element => {
-          this.listOfTopChats.push(element);
-        });
+        for (var x = 0; x < 3; x++) {
+          this.listOfTopChats.push(data[x]);
+        }
       })
   }
 
