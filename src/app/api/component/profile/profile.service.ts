d8f70a06587e3ed6930e116c87364a416f91da9a
Njabulo Thwala <njabulo.thwala@symbiotics.co.za>
2018-02-01 23:18:53 +0200

    "profile component points to environment files"

--- src/app/api/component/profile/profile.service.ts
+++ src/app/api/component/profile/profile.service.ts
@@ -3,18 +3,20 @@ import { Http, RequestMethod, Request, Headers, RequestOptions, } from '@angular
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Rx';
 import { Router } from '@angular/router';
+import { environment } from '../../../../environments/environment';
+
 @Injectable()
 export class ProfileService {
-  constructor(private http: Http, private router: Router) {}
+  constructor(private http: Http, private router: Router) { }
   getUserCurrentUserData() {
     const token = window.localStorage.getItem('token');
     const userId = window.localStorage.getItem('id');
-    const headers = new Headers({'authorization': token, 'content-type': 'application/json'});
+    const headers = new Headers({ 'authorization': token, 'content-type': 'application/json' });
     debugger;
-    return this.http.options('http://ec2-52-202-182-40.compute-1.amazonaws.com:3000/users/' + userId,{
-      headers: headers, 
+    return this.http.options(environment.host + environment.usersRoute + userId, {
+      headers: headers,
       method: 'GET'
-  })
-    .map(res => res.json());
+    })
+      .map(res => res.json());
   }
 }
